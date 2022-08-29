d5e9877b02637272ce17111ba1e2ce4a28a7c417
BGH76 <75952591+BGH76@users.noreply.github.com>
2022-08-29 15:55:27 -0400

    initial

---
+++ src/main/java/com/fullstackproject/GsFullStackProjectMediaSite/GsFullStackProjectMediaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackproject.GsFullStackProjectMediaSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsFullStackProjectMediaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsFullStackProjectMediaSiteApplication.class, args);
+	}
+
+}
